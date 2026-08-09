dcf1e24be84dbf63c5e2e0f580728540e9229307
Samuel Kuria <iamsamuelkuria@gmail.com>
2026-08-09 15:22:51 +0300

    feat: change

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -38,7 +38,7 @@ export const projectsData: Project[] = [
       "https://images.unsplash.com/photo-1672957603474-fe1d31c482e4?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     imageAiHint: "developer portfolio",
     technologies: ["HTML", "CSS", "JavaScript", "React"],
-    liveLink: "https://the-identified-blog.vercel.app",
+    liveLink: "https://mamihlapinatapai-litwfl.vercel.app",
   },
   {
     id: "project-3",
